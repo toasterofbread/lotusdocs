9a8c3bbe0efd9866e9c834b8273db5db87e909fd
Talo Halton <talohalton@gmail.com>
2023-12-17 23:37:20 +0000

    Fix favicon, remove duplicated mobile TOC list

--- assets/docs/js/toc-mobile-scrollspy.js
+++ assets/docs/js/toc-mobile-scrollspy.js
@@ -9,10 +9,15 @@ scrollArea.addEventListener("activate.bs.scrollspy", function(){
 tocBtn.addEventListener('shown.bs.dropdown', event => {
     tocBtn.style.borderBottom = 'none'
     tocBtn.style.borderRadius = '4px 4px 0 0'
-    // console.log("dropdown opened");
 })
 tocBtn.addEventListener('hidden.bs.dropdown', event => {
     tocBtn.style.borderBottom = '1px solid var(--alert-border-color)'
     tocBtn.style.borderRadius = '4px'
-    // console.log("dropdown closed");
 });
+
+let element = null
+do {
+    element = document.querySelector("#toc-mobile ul:not([class])")
+    element?.remove()
+}
+while (element != null)
